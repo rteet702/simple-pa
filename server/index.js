6e92081fa3bcb33fdb61202ec672bdcb22d19bfe
Robert Teets <teetsrobert@gmail.com>
2022-12-18 16:41:05 -0700

    chore: this time for sure

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
                 callback(new Error("Not allowed by CORS."));
             }
         },
-        methods: ["*"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
 app.use(cookieParser());
