d48f7b242cef156bb3c2410f5f339831d73d4fc6
Robert Teets <teetsrobert@gmail.com>
2022-12-19 19:48:17 -0700

    feat: finished conditional date text with icons.

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,10 +1,50 @@
 import axios from "axios";
 import React from "react";
 import { StyledButton } from "./StyledButton";
+import { AlertIcon } from "@primer/octicons-react";
 
 export const TaskCard = ({ task, setTasks }) => {
-    const date = new Date(task.dueAt);
-    const today = new Date();
+    const calculateDateDiff = () => {
+        const date = new Date(task.dueAt);
+        const today = new Date();
+        const dateVal = date.valueOf();
+        const todayVal = today.valueOf();
+
+        const diff = dateVal - todayVal;
+        const days = Math.trunc(diff / (1000 * 3600 * 24) + 1);
+
+        if (days === 0) {
+            return (
+                <div className="flex gap-5 items-center">
+                    <p className="text-amber-300">
+                        {date.toLocaleDateString()}
+                    </p>
+                    <AlertIcon
+                        verticalAlign="middle"
+                        size="medium"
+                        fill="#FCD453"
+                    />
+                </div>
+            );
+        } else if (days > 0) {
+            return (
+                <div className="flex gap-5">
+                    <p className>{date.toLocaleDateString()}</p>
+                </div>
+            );
+        } else {
+            return (
+                <div className="flex gap-5 items-center">
+                    <p className="text-red-500">{date.toLocaleDateString()}</p>
+                    <AlertIcon
+                        verticalAlign="middle"
+                        size="medium"
+                        fill="#f00"
+                    />
+                </div>
+            );
+        }
+    };
 
     const handleDelete = (e) => {
         e.preventDefault();
@@ -24,13 +64,7 @@ export const TaskCard = ({ task, setTasks }) => {
     return (
         <div className="w-full flex bg-white px-8 py-6 items-center  text-xl rounded-lg drop-shadow-sm hover:drop-shadow-lg transition-all cursor-pointer">
             <h4 className="text-2xl flex-1">{task.title}</h4>
-            {today < date ? (
-                <p className="text-right flex-1">
-                    Due {date.toLocaleDateString()}
-                </p>
-            ) : (
-                <p className="text-right text-red-500 flex-1">Due Today!</p>
-            )}
+            {calculateDateDiff()}
             <div className="flex gap-5 flex-1 justify-end">
                 <StyledButton theme="red" onClick={handleDelete}>
                     Delete
